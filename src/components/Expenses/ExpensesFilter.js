dc3bb2c0b0be1fa5cc8449513ca2afae473e73f2
Reemers <111338326+reemers@users.noreply.github.com>
2024-03-04 16:35:07 +0000

    Updated code

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,9 +1,12 @@
+import React from "react";
+
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
   const dropdownChangeHandler = (event) => {
     props.onChangeFilter(event.target.value);
   };
+
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
